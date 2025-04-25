5b0a125344355cd4940fc4ba062b53b2072fc7b7
Singh, Karanbir <Karanbir.Singh01@libertymutual.com>
2025-04-25 17:22:59 -0400

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+	jsxSingleQuote: true,
+	printWidth: 120,
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+};
